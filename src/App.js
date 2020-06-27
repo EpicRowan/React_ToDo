b2ea280858d2c192ae35b0657996c5cc86691591
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-27 13:12:41 -0700

    Created working todo app with state and data imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 
 import React from "react"
 import ToDoItem from "./components/ToDoItem"
-import ContactCard from "./components/ContactCard"
 import './index.css';
-import todosData from ""
+import todosData from "./todosData"
 
 // To Do list App
 
@@ -39,48 +38,4 @@ class App extends React.Component {
 }
 
 
-
-// //
-// Contact Card App
-
-// function App() {
-//   return(
-//     <div>
-//     <ContactCard 
-//     	name="Mr. Whiskerton"
-//     	imgURL="http://placekitten.com/300/200"
-//     	email="whiskers@catnip.com"
-//     	/>
-//     <ContactCard 
-//     	name="Destroyer of Worlds"
-//     	imgURL="http://placekitten.com/400/200"
-//     	email="DOW@catnip.com"
-//     	/>
-//     </div>
-//   )
-// }
-
-
-// class App extends React.Component {
-// 	constructor() {
-// 	super()
-// 	this.state = {
-// 		isLoggedIn: false
-// 	}
-// }
-// render() {
-// 	let wordDisplay
-// 	if (this.state.isLoggedIn) {
-// 		wordDisplay = "in"		
-// 	} else {
-// 		wordDisplay = "out"
-// 	}
-// 	return (
-// 		<div>
-// 			<h1> You're logged {wordDisplay}</h1>
-// 		</div>
-
-// 		)
-// }
-
-// }
+export default App
