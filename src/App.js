9452f89472e4b91d186416927a2fddcfd874b7c6
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-26 17:13:50 -0700

    Changing todo app to contact card app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
 import React from "react"
-import ToDoItem from "./components/ToDoItem"
+// import ToDoItem from "./components/ToDoItem"
 import './index.css';
+
+// To Do list App
+
+// function App() {
+//   return(
+//     <div>
+//       <ToDoItem />
+//       <ToDoItem />
+//       <ToDoItem />
+//     </div>
+//   )
+// }
+
+// Contact Card App
 
 function App() {
   return(
     <div>
-      <ToDoItem />
-      <ToDoItem />
-      <ToDoItem />
+
     </div>
   )
 }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+function ContactCard() {
+	return (
+		)
+}
+
+export default ContactCard
