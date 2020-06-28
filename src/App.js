32dc2a06204b65eeb451646abcbb42d11be271cc
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-27 22:52:31 -0700

    Created working todo app with event handler to allow for clicking on and off

--- src/App.js
+++ src/App.js
@@ -16,7 +16,31 @@ import todosData from "./todosData"
 //   )
 // }
 
-// To Do list App with state
+// // To Do list App with state
+
+// class App extends React.Component {
+// 	constructor() {
+// 		super()
+// 		this.state ={
+// 			todos: todosData
+// 		}
+		
+// 	}
+// 	render() {
+// 		const todothings= this.state.todos.map(item => <ToDoItem item={item}/>)
+
+// 		return (
+// 			<div>
+// 				{todothings}
+// 			</div>
+// 			)
+// 	}
+// }
+
+
+// export default App
+
+// To Do list App with state and event handler
 
 class App extends React.Component {
 	constructor() {
@@ -24,13 +48,31 @@ class App extends React.Component {
 		this.state ={
 			todos: todosData
 		}
-		
+	this.handleChange=this.handleChange.bind(this)
+
+	}
+
+	function handleChange(id) {
+		this.setState(prevState => {
+			const updatedTodos = prevState.map(todo => {
+				if (todo.id == id) {
+					todo.completed = !todo.completed
+				}
+				return todo
+			})
+		}
+			return {
+				todos: updatedTodos
+			}
+			)
 	}
 	render() {
-		const todothings= this.state.todos.map(item => <ToDoItem item={item}/>)
+		const todothings= this.state.todos.map(item => <ToDoItem item={item}
+			handleChange={this.handleChange}/>)
 
 		return (
 			<div>
+
 				{todothings}
 			</div>
 			)
