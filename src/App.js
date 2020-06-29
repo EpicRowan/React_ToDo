d629d76cfd6e3b0024f4057d1f2ba846efbc1eb3
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-06-28 19:24:10 -0700

    Minor bug fixes

--- src/App.js
+++ src/App.js
@@ -40,6 +40,45 @@ import todosData from "./todosData"
 
 // export default App
 
+// // To Do list App with state and event handler
+
+// class App extends React.Component {
+// 	constructor() {
+// 		super()
+// 		this.state ={
+// 			todos: todosData
+// 		}
+// 	this.handleChange=this.handleChange.bind(this)
+
+// 	}
+
+// 	handleChange(id) {
+// 		this.setState(prevState => {
+// 			const updatedTodos = prevState.todos.map(todo => {
+// 				if (todo.id === id) {
+// 					todo.completed = !todo.completed
+// 				}
+// 				return todo
+// 			})
+		
+// 			return {
+// 				todos: updatedTodos
+// 			}
+// 			})
+// 	}
+// 	render() {
+// 		const todothings= this.state.todos.map(item => <ToDoItem item={item}
+// 			handleChange={this.handleChange}/>)
+
+// 		return (
+// 			<div>
+
+// 				{todothings}
+// 			</div>
+// 			)
+// 	}
+// }
+
 // To Do list App with state and event handler
 
 class App extends React.Component {
@@ -53,22 +92,9 @@ class App extends React.Component {
 	}
 
 	handleChange(id) {
-		this.setState(prevState => {
-			const updatedTodos = prevState.todos.map(todo => {
-				if (todo.id === id) {
-					todo.completed = !todo.completed
-				}
-				return todo
-			})
 		
-			return {
-				todos: updatedTodos
-			}
-			})
-	}
 	render() {
-		const todothings= this.state.todos.map(item => <ToDoItem item={item}
-			handleChange={this.handleChange}/>)
+		const todothings= this.state.todos.map(item => <ToDoItem item={item}/>)
 
 		return (
 			<div>
@@ -78,6 +104,4 @@ class App extends React.Component {
 			)
 	}
 }
-
-
 export default App
