de52697a059e43809825f803898c92f407ddf871
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-07-04 14:20:29 -0700

    recreating form in React

--- src/App.js
+++ src/App.js
@@ -153,39 +153,70 @@
 
 // Forms practice
 
+// import React, {Component} from "react"
+
+// class App extends Component {
+// 	constructor() {
+// 		super()
+// 		this.state = {
+// 			firstName:""
+// 		}
+// 		this.handleChange = this.handleChange.bind(this)
+// 	}
+
+// 		handleChange(event) {
+// 			this.setState({
+// 				[event.target.name]: event.target.value
+// 			})
+// 		}
+
+// 		render() {
+// 			return (
+// 				<form>
+// 					<input type="text" 
+// 					 value={this.state.firstName}
+// 					 name="firstName"
+// 					 placeholder="First Name" 
+// 					 onChange={this.handleChange}/>
+// 					 <br />
+// 					<input type="text"
+// 					 value={this.state.lastName}
+// 					 name="lastName"
+// 					 placeholder="Last Name" 
+// 					 onChange={this.handleChange}/>
+// 					 <h1>{this.state.firstName} {this.state.lastName} </h1>
+// 				</form>
+// 				)
+// 		}
+// 	}
+
+// export default App
+
+// Rebuilding form from memory
+
 import React, {Component} from "react"
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			firstName:""
-		}
-		this.handleChange = this.handleChange.bind(this)
-	}
 
-		handleChange(event) {
-			this.setState({
-				[event.target.name]: event.target.value
-			})
-		}
-
-		render() {
-			return (
-				<form>
-					<input type="text" 
-					name="firstName"
-					 placeholder="First Name" 
-					 onChange={this.handleChange}/>
-					 <br />
-					<input type="text" 
-					name="lastName"
-					 placeholder="Last Name" 
-					 onChange={this.handleChange}/>
-					 <h1>{this.state.firstName} {this.state.lastName} </h1>
-				</form>
-				)
 		}
 	}
+	render() {
+		return (
+			<main>
+			<form>
+			<input placeholder="First Name"/> <br />
+			<input placeholder="Last Name"/> <br />
+			<input placeholder="Age"/> <br />
+
+			<br />
+			</form>
+			</main>
+
+			)
+	}
+}
 
 export default App
